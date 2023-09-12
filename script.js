fc388574d146b5ae29a757d748bdf32d49e3afbf
javdel3 <05JDcomputer@gmail.com>
2023-09-12 10:43:32 -0700

    All CSS & JS Finalized

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
-        if(this.scrollY > 30){
+        if(this.scrollY >= 30){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
-        if(this.scrollY > 75){
+        if(this.scrollY >= 75){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
